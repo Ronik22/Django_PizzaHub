59d7fdb7d67b50455daf06a9f5dbd1b3936d182b
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-04-20 00:30:01 +0530

    cart temp added

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Cart, Entry
 
 # Create your views here.
+
+def cart(request):
+    products = Entry.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, 'cart/cart.html', context)
+
