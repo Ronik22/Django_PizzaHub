2e036ee7c7c750ae136e5936c7668402a66b804b
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-04-27 00:58:16 +0530

    Cart modified

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import cart, add_to_cart
+from .views import cart, add_to_cart, checkout
 
 urlpatterns = [
     path('', cart, name="cart"),
     path('add-to-cart/', add_to_cart, name="add-to-cart"),
+    path('checkout/', checkout, name="checkout"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
+import json
 from django.contrib.auth.decorators import login_required
-from django.http.response import JsonResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
 from ecom.models import Product
 from django.shortcuts import render, get_object_or_404
@@ -39,3 +40,27 @@ def add_to_cart(request):
     if request.is_ajax():
         html = render_to_string('ecom/add_to_cart_section.html',context, request=request)
         return JsonResponse({'form':html})
+
+
+@login_required
+def checkout(request):
+    cart = get_object_or_404(Cart, user=request.user)
+    pdict = request.POST.get('checkoutip')
+    pdict = json.loads(pdict)
+    final_price = 0
+    
+    for keys,values in pdict.items():
+        product = get_object_or_404(Product, id=int(keys))
+        # Edit entry
+        currp = get_object_or_404(Entry, product=product, cart=cart)
+        currp.quantity = int(values)
+        currp.save()
+        # calc price
+        final_price = final_price + product.price * int(values)
+
+    final_price = final_price - 25
+    context = {
+        "pdict":pdict,
+        "fprice":final_price
+    }
+    return render(request,'cart/checkout.html', context)
