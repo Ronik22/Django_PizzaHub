c28c0c5d936a5477698bb1864cb4b85fe4709613
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-05-02 00:53:48 +0530

    Payment templates and routes updated

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import cart, add_to_cart, checkout, handle_checkout, payment, paymentsuccess
+from django.urls import path
+from .views import cart, add_to_cart, checkout, handle_checkout, handle_payment
 
 urlpatterns = [
     path('', cart, name="cart"),
     path('add-to-cart/', add_to_cart, name="add-to-cart"),
     path('checkout/', checkout, name="checkout"),
     path('handle_checkout/', handle_checkout, name="handle-checkout"),
-    # path('payment/<id>/', payment, name="payment"),
-    path('paymentsuccess/', paymentsuccess, name="paymentsuccess"),
+    path('handle_payment/', handle_payment, name="handle-payment"),
 ]
